df6edfe57cca505b8fa17d3096677b61b0619e7a
Osama Sayed <osamasayed151@gmail.com>
2021-11-05 20:15:10 +0200

    fix issue

--- src/GeorgeAndAccommodation.java
+++ src/GeorgeAndAccommodation.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 
 public class GeorgeAndAccommodation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int size = scanner.nextInt();
+		int counter = 0;
+		for (int i=0; i < size; i++){
+			int num1 = scanner.nextInt();
+			int num2 = scanner.nextInt();
+			if (num2 - num1 >= 2){
+				counter++;
+			}
+		}
+		System.out.println(counter);
 
 	}
 
