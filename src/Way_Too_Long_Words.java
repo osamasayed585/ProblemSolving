82efe1121494f3a12126194676f4e78936aded47
Osama Sayed <osamasayed151@gmail.com>
2021-11-05 21:14:28 +0200

    new problem "way too long words"

--- src/Way_Too_Long_Words.java
+++ src/Way_Too_Long_Words.java
@@ -5,6 +5,13 @@ public class Way_Too_Long_Words {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
-        int text ; 
+        for (int i=0; i<size; i++){
+            String word = scanner.next();
+            if (word.toCharArray().length > 10){
+                System.out.println(word.toCharArray()[0] +""+ (word.toCharArray().length-2) + word.toCharArray()[word.toCharArray().length-1]);
+                continue;
+            }
+            System.out.println(word);
+        }
     }
 }
