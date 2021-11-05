88d174f01e6d56817ce200c95fdac2a56501036d
Osama Sayed <osamasayed151@gmail.com>
2021-11-05 20:21:30 +0200

    fix issue

--- src/Summation_from_1_To_N.java
+++ src/Summation_from_1_To_N.java
@@ -1,17 +1,17 @@
-//import java.util.Scanner;
-//
-//public class Summation_from_1_T_oN {
-//	
-//	public static void main(String[] args) {
-//		
-//		Scanner scanner = new Scanner(System.in);
-//		long num = scanner.nextLong();
-//		  // sum = num * (num +1) / 2
-//		long sum = num * (num + 1) / 2;
-//		System.out.println(" sum = "+sum);
-//		}
-//
-//	}
+import java.util.Scanner;
+
+class Summation_from_1_T_oN {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		long num = scanner.nextLong();
+		  // sum = num * (num +1) / 2
+		long sum = num * (num + 1) / 2;
+		System.out.println(" sum = "+sum);
+		}
+
+	}
 		
 	
 	
