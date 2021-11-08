c96fa5047cd9fbd8d107585b6ff982a18f8d433a
Osama Sayed <osamasayed151@gmail.com>
2021-11-08 02:06:30 +0200

    this is a new problem "Another One Bites The Dust"

--- src/Another_One_Bites_The_Dust.java
+++ src/Another_One_Bites_The_Dust.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+
 public class Another_One_Bites_The_Dust {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
 
 }
