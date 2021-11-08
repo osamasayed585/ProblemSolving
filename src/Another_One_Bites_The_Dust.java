4e90a8bbdaf73f86746f48bd7e8154be0a447942
Osama Sayed <osamasayed151@gmail.com>
2021-11-08 02:46:25 +0200

    this is a new problem "Another One Bites The Dust" in time 155 ms

--- src/Another_One_Bites_The_Dust.java
+++ src/Another_One_Bites_The_Dust.java
@@ -4,7 +4,18 @@ public class Another_One_Bites_The_Dust {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        long num1 = scanner.nextInt();
+        long num2 = scanner.nextInt();
+        long num3 = scanner.nextInt();
 
+        long a = num1 + num3;
+        long b = num2 + num3;
+        if (a > b){
+            System.out.println((b*2) +1);
+        }else if (b > a) {
+            System.out.println((a*2) + 1);
+        }else {
+            System.out.println(a * 2);
+        }
     }
-
 }
