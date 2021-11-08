ff43c59903a8634a4ec7922b597dcc40df994036
Osama Sayed <osamasayed151@gmail.com>
2021-11-09 00:20:55 +0200

    this is a new problem "Domino piling"

--- src/DominoPiling.java
+++ src/DominoPiling.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Dominopiling {
+public class DominoPiling {
     public static void main(String[] args) {
         Scanner scanner = new Scanner((System.in));
         int size = scanner.nextInt();
