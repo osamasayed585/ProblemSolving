d9adb60da1c6d97d4c9f870662fbc75da9da32e6
Osama Sayed <osamasayed151@gmail.com>
2021-11-09 00:32:31 +0200

    this is a new problem "Domino piling" in time 248 ms

--- src/DominoPiling.java
+++ src/DominoPiling.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class DominoPiling {
     public static void main(String[] args) {
         Scanner scanner = new Scanner((System.in));
-        int size = scanner.nextInt();
+        int num = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.println((num * num2) / 2);
     }
 }
