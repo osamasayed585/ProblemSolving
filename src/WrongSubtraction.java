59d784a0d52ea890736c28ed84844a7b1d69d4e0
Osama Sayed <osamasayed151@gmail.com>
2021-11-09 01:18:55 +0200

    this is a new problem "Wrong Subtraction"

---
+++ src/WrongSubtraction.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class WrongSubtraction {
+
+    // link the problem in Codeforces
+    // https://codeforces.com/problemset/problem/977/A
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
