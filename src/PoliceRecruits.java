d5e02e106e5e3b532e816191fe81ff38d7353e52
Osama Sayed <osamasayed151@gmail.com>
2021-11-09 00:53:05 +0200

    this is a new problem "Police Recruits" in time

---
+++ src/PoliceRecruits.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PoliceRecruits {
+
+    // link the problem in codeforces
+    // https://codeforces.com/problemset/problem/427/A
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
