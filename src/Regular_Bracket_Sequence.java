b26a7fb8de19a7dac9b4e0fc2075a4f63d684b23
Osama Sayed <osamasayed151@gmail.com>
2021-11-08 02:03:08 +0200

    this is a new problem "Regular Bracket Sequence" in time 374 ms

--- src/Regular_Bracket_Sequence.java
+++ src/Regular_Bracket_Sequence.java
@@ -4,5 +4,22 @@ public class Regular_Bracket_Sequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        char [] regular = scanner.next().toCharArray();
+        int size = regular.length;
+        int counter = 0;
+
+        for (int i=0; i<regular.length; i++){
+
+            if (regular[i] == '('){
+                counter++;
+            }else {
+                counter--;
+                if (counter < 0 ){
+                    size--;
+                    counter = 0;
+                }
+            }
+        }
+        System.out.println(size - counter);
     }
 }
