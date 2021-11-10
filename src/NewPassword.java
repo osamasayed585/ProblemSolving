02a273b3736dfda9f9c0bc78fe5672260e6d468f
Osama Sayed <osamasayed151@gmail.com>
2021-11-10 12:22:36 +0200

    this a new problem "New password" in time 124 ms

--- src/NewPassword.java
+++ src/NewPassword.java
@@ -7,6 +7,19 @@ public class NewPassword {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        int letters = scanner.nextInt();
+        String password = "";
 
+        for (int i=97; i<97 + letters ; i++){
+            password = password + (char)i;
+        }
+        for (int i=0; i<length; i++){
+            if(password.toCharArray().length != length){
+                password = password + password.toCharArray()[i];
+            }
+        }
+        System.out.println(password);
     }
+
 }
