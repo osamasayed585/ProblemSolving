a6c911a8cf65f16e12a326225c48ae75456eab61
Osama Sayed <osamasayed151@gmail.com>
2021-11-10 10:10:16 +0200

    this a new problem "Decoding"

---
+++ src/Decoding.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Decoding {
+
+    // this  is a link problem in CodeForces website
+    // https://codeforces.com/problemset/problem/746/B
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+    }
+
+}
