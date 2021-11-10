99b4ab15a099e2ec72e4626adeeaad5a4ebdc89d
Osama Sayed <osamasayed151@gmail.com>
2021-11-10 10:46:36 +0200

    this a new problem "New password"

---
+++ src/NewPassword.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class NewPassword {
+
+    // this  is a link problem in CodeForces website
+    // https://codeforces.com/problemset/problem/770/A
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
