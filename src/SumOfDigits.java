f90ef4ad234dc6ee0b1b979980250daeaa6fd4b9
Osama Sayed <osamasayed151@gmail.com>
2021-11-11 23:38:01 +0200

    This a new problem "Sum of Digits" int time 278 ms

--- src/SumOfDigits.java
+++ src/SumOfDigits.java
@@ -7,5 +7,30 @@ public class SumOfDigits {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String num = scanner.next();  // 9 9 1
+        if (num.length() == 1) {
+            System.out.println(0);
+            return;
+        }
+        char[] digits = num.toCharArray();  // {9,9,1}
+        int sum =0;
+
+        for (int i = 0; i < num.length(); i++) {
+            sum += digits[i] - '0';
+        }
+        int counter = 1;
+        while (sum >= 10){
+            sum = sum(sum);
+            counter++;
+        }
+        System.out.println(counter);
+    }
+    public static int sum(int num){
+        int result = 0;
+        while (num != 0){
+            result += num % 10;
+            num /= 10;
+        }
+        return result;
     }
 }
