3c5e03403cacd3b0d44225c662603c931a1dec69
Osama Sayed <osamasayed151@gmail.com>
2021-11-11 19:59:58 +0200

    this a new problem "Lovely Palindromes" in time 420 ms

--- src/LovelyPalindromes.java
+++ src/LovelyPalindromes.java
@@ -7,6 +7,12 @@ public class LovelyPalindromes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String num = scanner.next();
+        char[] numbers = num.toCharArray();
+        System.out.print(num);
 
+        for (int i= numbers.length-1; i>=0; i--){
+            System.out.print(numbers[i]);
+        }
     }
 }
