365c39bfe56f283136defcabdc12a72a7c77ba38
Osama Sayed <osamasayed151@gmail.com>
2021-11-11 19:58:22 +0200

    this a new problem "Lovely Palindromes"

---
+++ src/LovelyPalindromes.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class LovelyPalindromes {
+
+    // this is link the problem in codeforces
+    // https://codeforces.com/contest/688/problem/B
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
