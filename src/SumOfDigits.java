cd4c64dd8a830bff1dc8e5d6ac404ce31dfa3e50
Osama Sayed <osamasayed151@gmail.com>
2021-11-11 20:08:56 +0200

    This a new problem "Sum of Digits" add the link

--- src/SumOfDigits.java
+++ src/SumOfDigits.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class SumOfDigits {
 
+    // this is a link the problem in codeForces
+    // https://codeforces.com/problemset/problem/102/B
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
     }
